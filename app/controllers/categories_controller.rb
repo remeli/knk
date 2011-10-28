b07feb45cbc766c2b0cb24c3d7f9bc68acb9f5bf
Kusainov Yerlan <lagox.mail@gmail.com>
2011-10-28 15:26:15 +0600

    add path functional for catalog

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,5 @@
 class CategoriesController < ApplicationController
-  before_filter :load_category
+  before_filter :load_category, :load_path
   
   def show
     @products = @category.products.published.page(params[:page]).per(30)
@@ -10,10 +10,27 @@ class CategoriesController < ApplicationController
     
     def load_category
       begin
-        @categories = Category.all
+        @main_categories = Category.mains
         @category = Category.find(params[:id])        
       rescue ActiveRecord::RecordNotFound
         redirect_to products_path, :alert => "Категория не найдена"        
+      end
+    end
+    
+    def load_path
+      @path = []
+      @current_category = Category.find(params[:id])
+      if @current_category.parent
+        @path << @current_category.parent
+        for i in (0..8)
+          cat = @path[i]
+          unless cat.parent_id == nil
+            prev = Category.find(cat.parent_id)
+            @path << prev
+          else
+            break
+          end
+        end  
       end
     end
     
