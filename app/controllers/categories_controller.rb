ce6c89b113a1f71c4fe88c198f50fa6aa0929845
Kusainov Yerlan <lagox.mail@gmail.com>
2011-11-02 13:58:17 +0600

    edit

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,12 +1,35 @@
 class CategoriesController < ApplicationController
   before_filter :load_category, :load_path
-    
+  
+  $categories = []
+  
   def show
     @title = "Каталог / #{@category.name}"
     @products = @category.products.published.page(params[:page]).per(30)
+    # if @products.size == 0
+    #   page, per_page = 1, 1
+    #   @products = Array.new
+    #   @allcat = find_all_cat(@category)
+    #   @allcat.each do |cat|
+    #     for i in (0..cat.products.size-1)
+    #       @products << cat.products[i]
+    #     end
+    #   end
+    # end
   end
   
   private
+    
+    def find_all_cat(category)
+      if category.children.size > 0
+        category.children.each do |subcat|
+          $categories << subcat
+          if subcat.children.size > 0
+            find_all_cat(subcat)
+          end
+        end
+      end
+    end
     
     def load_category
       begin
